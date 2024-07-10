30ba665c3c8c007a3e9dab799301ee57c5d100d7
shahnawaz <shahnawaz.hussain@ganitinc.com>
2024-07-10 12:00:55 +0530

    Animation added while open or close menu

--- javascript/hamburger.js
+++ javascript/hamburger.js
@@ -1,14 +1,23 @@
-//  when click on menu icon
-
 function handleCloseMenu() {
     const filtersMenu = document.querySelector('.filters-menu');
     filtersMenu.classList.remove('show-filters-menu');
+    
+    // After transition complete
+    filtersMenu.addEventListener('transitionend', function() {
+        if (!filtersMenu.classList.contains('show-filters-menu')) {
+            filtersMenu.style.display = 'none';
+        }
+    }, { once: true });
+
     document.body.classList.remove('no-scroll');
 }
 
 function handleOpenMenu() {
-    const filtersMenu = document.getElementById('filters-menu');
+    const filtersMenu = document.querySelector('.filters-menu');
     const body = document.body;
+
+    filtersMenu.style.display = 'flex';
+    filtersMenu.offsetHeight;
     filtersMenu.classList.add('show-filters-menu');
     body.classList.add('no-scroll');
 }
